8d169965490e8fa0daa16aeb3a7a9a5f8de61fdb
imam1788 <imamhossain1788@gmail.com>
2025-06-17 18:05:35 +0600

    changed Gallery page for importing image

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -2,17 +2,23 @@ import "yet-another-react-lightbox/styles.css";
 import Lightbox from "yet-another-react-lightbox";
 import { useState } from "react";
 
+import gallery1 from '../assets/gallery/gallery1.jpg'
+import gallery2 from '../assets/gallery/gallery2.jpg'
+import gallery3 from '../assets/gallery/gallery3.jpg'
+import gallery4 from '../assets/gallery/gallery4.jpg'
+import gallery5 from '../assets/gallery/gallery5.jpg'
+import gallery6 from '../assets/gallery/gallery6.jpg'
+import gallery7 from '../assets/gallery/gallery7.jpg'
+import gallery8 from '../assets/gallery/gallery8.jpg'
+import gallery9 from '../assets/gallery/gallery9.jpg'
+import gallery10 from '../assets/gallery/gallery10.jpg'
+
 const images = [
-  "/src/assets/gallery/gallery1.jpg",
-  "/src/assets/gallery/gallery2.jpg",
-  "/src/assets/gallery/gallery3.jpg",
-  "/src/assets/gallery/gallery4.jpg",
-  "/src/assets/gallery/gallery5.jpg",
-  "/src/assets/gallery/gallery6.jpg",
-  "/src/assets/gallery/gallery7.jpg",
-  "/src/assets/gallery/gallery8.jpg",
-  "/src/assets/gallery/gallery9.jpg",
-  "/src/assets/gallery/gallery10.jpg",
+  gallery1,
+  gallery2,
+  gallery3,
+  gallery4,
+  gallery5,gallery6,gallery7, gallery8, gallery9,gallery10
 ];
 
 const Gallery = () => {
